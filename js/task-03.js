c80d0c7d5370c803fb713fd7a7eea9b697b328e6
Dmytro Petrenko <blinker182ca@gmail.com>
2020-10-06 14:04:10 +0300

    fixed frequent reference to DOM
    done few variants

--- js/task-03.js
+++ js/task-03.js
@@ -21,17 +21,46 @@ const images = [
 const gallery = document.querySelector('#gallery');
 gallery.classList.add('gallery');
 
-const imagesList = images.map(image => {
-  const galleryItemEl = document.createElement('li');
+// --------------------- 1ый (плохой) вариант ---------------------
+// -------------- Обращение к DOM на каждой иттерации
 
-  galleryItemEl.insertAdjacentHTML(
-    'beforeend',
-    `<img class='gallery-img' src="${image.url}" alt="${image.alt}">`,
-  );
+// const imagesList = images.map(image => {
+//   const galleryItemEl = document.createElement('li');
 
-  galleryItemEl.classList.add('gallery-item');
-  return galleryItemEl;
+//   galleryItemEl.insertAdjacentHTML(
+//     'beforeend',
+//     `<img class='gallery-img' src="${image.url}" alt="${image.alt}">`,
+//   );
+
+//   galleryItemEl.classList.add('gallery-item');
+//   return galleryItemEl;
+// });
+
+// gallery.append(...imagesList);
+// console.log(gallery);
+
+// --------------------- 2ой вариант ---------------------
+
+const imagesList = images.map(({ url, alt }) => {
+  return `<li class="gallery-item">
+              <img class="gallery-img" src="${url}" alt="${alt}"></img>
+          </li>`;
 });
 
-gallery.append(...imagesList);
+gallery.insertAdjacentHTML('beforeend', imagesList);
 console.log(gallery);
+
+// --------------------- 3ий вариант ---------------------
+
+// const galleryMarkup = images.map(({ url, alt }) => {
+//   const imagesList = gallery.insertAdjacentHTML(
+//     'beforeend',
+//     `<li class="gallery-item">
+//         <img class='gallery-img' src="${url}" alt="${alt}"</img>
+//       </li>`,
+//   );
+
+//   return imagesList;
+// });
+
+// console.log(gallery);
